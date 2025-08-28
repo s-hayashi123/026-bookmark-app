8b05ba6be021e16da909e51da779b2680ed8b9c0
s-hayashi123 <shake7602@gmail.com>
2025-08-29 06:23:20 +0900

    prisma修正

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,25 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaLibSQL } from "@prisma/adapter-libsql";
-import { createClient } from "@libsql/client";
 
-let prisma: PrismaClient;
+const adapter = new PrismaLibSQL({
+  url: process.env.TURSO_DATABASE_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN!,
+});
 
-if (process.env.NODE_ENV === "production") {
-  // Turso に接続するのは本番だけ
-
-  const libsql = createClient({
-    url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
-  });
-
-  const adapter = new PrismaLibSQL({
-    url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
-  });
-  prisma = new PrismaClient({ adapter });
-} else {
-  // ローカルは普通の SQLite
-  prisma = new PrismaClient();
-}
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
